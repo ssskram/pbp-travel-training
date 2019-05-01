85fa5b18cca04c57655851839ee0586fb44c1587
ssskram <paul.marks@pittsburghpa.gov>
2019-05-01 11:24:54 -0400

    confirmationEmail func

--- confirmSubmission/index.js
+++ confirmSubmission/index.js
@@ -7,25 +7,50 @@
     @param {req.query.createdBy} string - User's email address
 */
 
-const fetch = require('node-fetch')
-const fs = require('fs')
-const getTravelTraining = require('../shared/getTravelTraining')
-const sendEmail = require('../shared/sendEmail')
-require('../shared/stringFormat')
-global.Headers = fetch.Headers
+const fetch = require("node-fetch");
+const fs = require("fs");
+const dateFormat = require("dateformat");
+const getTravelTraining = require("../shared/getTravelTraining");
+const sendEmail = require("../shared/sendEmail");
+require("../shared/stringFormat");
+global.Headers = fetch.Headers;
 
 module.exports = async (context, req) => {
+  // return immediately
+  context.res = {
+    status: 202
+  };
 
-    // return confirmation immediately
-    context.res = {
-        status: 202
-    }
+  const itemID = req.query.itemID;
+  const createdBy = req.query.createdBy;
+  const df = "mm/dd/yyyy";
 
-    const itemID = req.query.itemID
-    const createdBy  = req.query.createdBy
+  // first, get Travel/Training req
+  const travelTraining = await getTravelTraining(itemID);
 
-    // first, get Travel/Training req
+  // send confirmation email
 
-    // send confirmation email
+  // base sendgrid load
+  let load = {
+    to: createdBy,
+    from: {
+      email: createdBy,
+      name: "Travel/Training Requests",
+    },
+    subject: "Travel/Training Request Submitted"
+  };
 
-}
+  // read in email template, populate with data, and send
+  const path = __dirname + "//emailTemplate/confirmationEmail.html";
+  fs.readFile(path, "utf8", async (err, data) => {
+    load.html = await String.format(
+      data,
+      travelTraining.officerName, // 0
+      travelTraining.trainingRequested, // 1
+      dateFormat(travelTraining.startDate, df), // 2
+      dateFormat(travelTraining.endDate, df), // 3
+      dateFormat(travelTraining.submitted, df) // 4
+    );
+    await sendEmail(load);
+  });
+};
--- shared/getTravelTraining.js
+++ shared/getTravelTraining.js
@@ -1,17 +1,45 @@
 const fetch = require("node-fetch");
 
 async function getTravelTraining(itemID) {
-  const call = await fetch(
-    "https://365proxy.azurewebsites.us/travelTrainings/item?itemID=" +
-      itemID +
-      "&key=" +
-      process.env.PROXY,
-    {
-      method: "get"
-    }
-  );
-  const item = await call.json();
-  return item;
+  // const call = await fetch(
+  //   "https://365proxy.azurewebsites.us/travelTrainings/item?itemID=" +
+  //     itemID +
+  //     "&key=" +
+  //     process.env.PROXY,
+  //   {
+  //     method: "get"
+  //   }
+  // );
+  // const item = await call.json();
+  // return item;
+
+  return {
+    id: 2000,
+    url:
+      "https://cityofpittsburgh.sharepoint.com/sites/Police/_layouts/15/FormServer.aspx?XmlLocation=/sites/Police/TravelTraining/TravelTraining_Linda Barone_2017-10-11T16_04_50.xml&ClientInstalled=false&DefaultItemOpen=1&Source=https%3A%2F%2Fcityofpittsburgh%2Esharepoint%2Ecom%2Fsites%2FPolice%2FTravelTraining%2FForms%2FAllItems%2Easpx",
+    officerName: "Steven Schultze",
+    trainingRequested: "Major Cities Technology Committee",
+    startDate: "2019-10-18T04:00:00Z",
+    endDate: "2019-10-20T04:00:00Z",
+    submitted: "2019-04-11T20:04:51Z",
+    amountRequested: "$1077.45",
+    firstForward: "paul.marks@pittsburghpa.gov",
+    firstApprovalSignature: null,
+    secondForward: null,
+    secondApprovalSignature: null,
+    thirdForward: null,
+    thirdApprovalSignature: null,
+    fourthForward: null,
+    fourthApprovalSignature: null,
+    fifthForward: null,
+    fifthApprovalSignature: null,
+    finalForward: null,
+    finalApprovalSignature: null,
+    lastNotice: "2017-11-06T05:00:00Z",
+    finalDenial: null,
+    finalApproval: null,
+    daysOnly: null
+  };
 }
 
-module.exports = getTravelTraining
+module.exports = getTravelTraining;
