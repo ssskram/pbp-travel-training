676c1d2a8e461de5dae63f2e93f74df429526d33
ssskram <paul.marks@pittsburghpa.gov>
2019-05-02 13:02:05 -0400

    approval status endpoint

--- approvalStatus/index.js
+++ approvalStatus/index.js
@@ -5,7 +5,7 @@
     final approval (full),
     or rejection.
 
-    Then, emails the relevant parties with necessary information
+    Then, emails the relevant parties with notice of completion
 
     @param {req.query.itemID} number
     @param {req.query.createdBy} string - User's email address
@@ -26,10 +26,167 @@ module.exports = async (context, req) => {
 
   const itemID = req.query.itemID;
   const createdBy = req.query.createdBy;
+  const df = "mm/dd/yyyy";
 
   // first, get Travel/Training req
+  const tt = await getTravelTraining(itemID);
 
   // check for completion
+  if (tt.finalApproval == "Yes" && tt.daysOnly != "Yes") {
+    fullApproval();
+  } else if (tt.finalApproval == "Yes" && tt.daysOnly == "Yes") {
+    daysOnly();
+  } else if (tt.finalDenial == "Yes") {
+    rejection();
+  }
 
   // send necessary emails
+  async function fullApproval() {
+    // email approvers
+    let approvers = {
+      to: [
+        tt.firstForward,
+        tt.secondForward,
+        tt.thirdForward,
+        tt.fourthForward,
+        tt.fifthForward,
+        tt.finalForward
+      ],
+      from: {
+        email: createdBy,
+        name: "Travel/Training Requests"
+      },
+      subject: "Training/Travel Approved"
+    };
+    const path = __dirname + "//emailTemplates/fullApproval.html";
+    fs.readFile(path, "utf8", async (err, data) => {
+      load.html = await String.format(
+        data,
+        tt.officerName, // 0
+        dateFormat(tt.submitted, df), // 1
+        tt.amountRequested, // 2
+        tt.url // 3
+      );
+      await sendEmail(approvers);
+    });
+
+    // then, email submitter
+    let submitter = {
+      to: createdBy,
+      from: {
+        email: createdBy,
+        name: "Travel/Training Requests"
+      },
+      subject: "Training/Travel Approved"
+    };
+    const path = __dirname + "//emailTemplates/fullApprovalSubmitter.html";
+    fs.readFile(path, "utf8", async (err, data) => {
+      load.html = await String.format(
+        data,
+        tt.officerName, // 0
+        dateFormat(tt.submitted, df), // 1
+        tt.amountRequested, // 2
+        tt.url // 3
+      );
+      await sendEmail(submitter);
+    });
+  }
+
+  async function daysOnly() {
+    // email approvers
+    let approvers = {
+      to: [
+        tt.firstForward,
+        tt.secondForward,
+        tt.thirdForward,
+        tt.fourthForward,
+        tt.fifthForward,
+        tt.finalForward
+      ],
+      from: {
+        email: createdBy,
+        name: "Travel/Training Requests"
+      },
+      subject: "Training/Travel Approved"
+    };
+    const path = __dirname + "//emailTemplates/daysOnly.html";
+    fs.readFile(path, "utf8", async (err, data) => {
+      load.html = await String.format(
+        data,
+        tt.officerName, // 0
+        dateFormat(tt.submitted, df), // 1
+        tt.url // 2
+      );
+      await sendEmail(approvers);
+    });
+
+    // then, email submitter
+    let submitter = {
+      to: createdBy,
+      from: {
+        email: createdBy,
+        name: "Travel/Training Requests"
+      },
+      subject: "Training/Travel Approved"
+    };
+    const path = __dirname + "//emailTemplates/daysOnlySubmitter.html";
+    fs.readFile(path, "utf8", async (err, data) => {
+      load.html = await String.format(
+        data,
+        tt.officerName, // 0
+        dateFormat(tt.submitted, df), // 1
+        tt.url // 2
+      );
+      await sendEmail(submitter);
+    });
+  }
+
+  async function rejection() {
+    // email approvers
+    let approvers = {
+      to: [
+        tt.firstForward,
+        tt.secondForward,
+        tt.thirdForward,
+        tt.fourthForward,
+        tt.fifthForward,
+        tt.finalForward
+      ],
+      from: {
+        email: createdBy,
+        name: "Travel/Training Requests"
+      },
+      subject: "Training/Travel Rejected"
+    };
+    const path = __dirname + "//emailTemplates/rejected.html";
+    fs.readFile(path, "utf8", async (err, data) => {
+      load.html = await String.format(
+        data,
+        tt.officerName, // 0
+        dateFormat(tt.submitted, df), // 1
+        tt.url // 2
+      );
+      await sendEmail(approvers);
+    });
+
+    // then, email submitter
+    let submitter = {
+      to: createdBy,
+      from: {
+        email: createdBy,
+        name: "Travel/Training Requests"
+      },
+      subject: "Training/Travel Rejected"
+    };
+    const path = __dirname + "//emailTemplates/rejectedSubmitter.html";
+    fs.readFile(path, "utf8", async (err, data) => {
+      load.html = await String.format(
+        data,
+        tt.officerName, // 0
+        dateFormat(tt.submitted, df), // 1
+        tt.url // 2
+      );
+      await sendEmail(submitter);
+    });
+  }
 };
