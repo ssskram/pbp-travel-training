efe1720530d0a5d6345295768d518dc2293efded
ssskram <paul.marks@pittsburghpa.gov>
2019-06-27 14:30:53 -0400

    .

--- shared/functions/setLastNotice.js
+++ shared/functions/setLastNotice.js
@@ -3,19 +3,19 @@ const fetch = require("node-fetch");
 async function setLastNotice(itemID, lastNotice) {
   console.log(itemID);
   console.log(lastNotice);
-  // await fetch(
-  //   "https://365proxy.azurewebsites.us/travelTrainings/setLastNotice?itemID=" +
-  //     itemID +
-  //     "&lastNotice=" +
-  //     lastNotice,
-  //   {
-  //     method: "POST",
-  //     headers: new Headers({
-  //       Authorization: "Bearer " + process.env.PROXY,
-  //       "Content-type": "application/json"
-  //     })
-  //   }
-  // );
+  await fetch(
+    "https://365proxy.azurewebsites.us/travelTrainings/setLastNotice?itemID=" +
+      itemID +
+      "&lastNotice=" +
+      lastNotice,
+    {
+      method: "POST",
+      headers: new Headers({
+        Authorization: "Bearer " + process.env.PROXY,
+        "Content-type": "application/json"
+      })
+    }
+  );
 }
 
 module.exports = setLastNotice;
